d8606a719b274ecd268f7a8ccd30cd77c6459105
HysenChan <HysenChan@163.com>
2024-12-16 17:08:45 +0800

    Update Script_03_06_Main.cs

--- Unity2023.2.20f1c1/Unity3dDesign/Unity3dDesign/Assets/Scripts/Chapter03/Script_03_06_Main.cs
+++ Unity2023.2.20f1c1/Unity3dDesign/Unity3dDesign/Assets/Scripts/Chapter03/Script_03_06_Main.cs
@@ -10,4 +10,21 @@ public class Script_03_06_Main : MonoBehaviour
             Debug.LogFormat($"name : {info.name} id : {info.id}");
         }
     }
+
+    [Header("Header1")]
+    public int a;
+    public float b;
+
+    [Header("Header2")]
+    public int a2;
+    public float b2;
+
+    public enum Type
+    {
+        [InspectorName("第一")]
+        One,
+        [InspectorName("第二")]
+        Two,
+    }
+    public Type type;
 }
