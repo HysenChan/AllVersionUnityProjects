4ff8ea4073172125187f13e0c6420421f764bb36
HysenChan <HysenChan@163.com>
2024-12-17 17:06:22 +0800

    协程，最后一帧统一调用更新值。

--- Unity2023.2.20f1c1/Unity3dDesign/Unity3dDesign/Assets/Scripts/Chapter03/Script_03_08.cs
+++ Unity2023.2.20f1c1/Unity3dDesign/Unity3dDesign/Assets/Scripts/Chapter03/Script_03_08.cs
@@ -6,38 +6,67 @@ using UnityEngine.UIElements.Experimental;
 
 public class Script_03_08 : MonoBehaviour
 {
-    private void Start()
+    //private void Start()
+    //{
+    //    //启动协程任务
+    //    StartCoroutine(MyFunction());
+    //    //关闭协程
+    //    StopCoroutine(MyFunction());
+    //    //关闭所有携程任务
+    //    StopAllCoroutines();
+
+    //    GetComponent<Script_03_08>().StartCoroutine(MyFunction());
+
+    //    Resources.LoadAsync<Sprite>("icon").completed += (a) =>
+    //    {
+    //        //加载完成1
+    //    };
+
+    //    Resources.LoadAsync<TextAsset>("text").completed += (a) =>
+    //    {
+    //        //加载完成2
+    //    };
+    //}
+
+    //IEnumerator MyFunction()
+    //{
+    //    Debug.Log("MyFunction");
+    //    yield return null;
+    //}
+
+    //IEnumerator Load()
+    //{
+    //    yield return Resources.LoadAsync<Sprite>("icon");//加载完成1
+    //    yield return Resources.LoadAsync<TextAsset>("text");//加载完成2
+    //    Debug.Log("此时1和2都加载完成了");
+    //}
+
+    private int m_CacheValue = -1;
+    public void UpdateValue(int value)
     {
-        //启动协程任务
-        StartCoroutine(MyFunction());
-        //关闭协程
-        StopCoroutine(MyFunction());
-        //关闭所有携程任务
-        StopAllCoroutines();
-
-        GetComponent<Script_03_08>().StartCoroutine(MyFunction());
-
-        Resources.LoadAsync<Sprite>("icon").completed += (a) =>
+        Debug.Log($"第{Time.frameCount}帧尝试复制{value}");
+        if (m_CacheValue == -1)
         {
-            //加载完成1
-        };
-
-        Resources.LoadAsync<TextAsset>("text").completed += (a) =>
-        {
-            //加载完成2
-        };
+            StartCoroutine(Wait());
+        }
+        m_CacheValue = value;
     }
 
-    IEnumerator MyFunction()
+    IEnumerator Wait()
     {
-        Debug.Log("MyFunction");
-        yield return null;
+        yield return new WaitForEndOfFrame();
+        //用最后一次的m_CacheValue进行耗时计算
+        Debug.Log($"第{Time.frameCount}帧最终处理值{m_CacheValue}");
+        m_CacheValue = -1;
     }
 
-    IEnumerator Load()
+    private void Update()
     {
-        yield return Resources.LoadAsync<Sprite>("icon");//加载完成1
-        yield return Resources.LoadAsync<TextAsset>("text");//加载完成2
-        Debug.Log("此时1和2都加载完成了");
+        if (Input.GetMouseButtonDown(0))
+        {
+            //尝试进行多次赋值
+            UpdateValue(2);
+            UpdateValue(3);
+        }
     }
 }
