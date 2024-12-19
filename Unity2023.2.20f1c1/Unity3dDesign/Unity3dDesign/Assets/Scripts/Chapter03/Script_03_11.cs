f321e79a503a8c1f83a156f92d548dea11c62853
HysenChan <HysenChan@163.com>
2024-12-19 16:44:15 +0800

    动态加载游戏对象和修改材质资源

--- Unity2023.2.20f1c1/Unity3dDesign/Unity3dDesign/Assets/Scripts/Chapter03/Script_03_11.cs
+++ Unity2023.2.20f1c1/Unity3dDesign/Unity3dDesign/Assets/Scripts/Chapter03/Script_03_11.cs
@@ -13,9 +13,19 @@ public class Script_03_11 : MonoBehaviour
 {
     private void Start()
     {
-        GameObject go = new GameObject("Cube");
-        go.transform.position = Vector3.zero;
-        go.AddComponent<MeshFilter>().mesh = Resources.GetBuiltinResource<Mesh>("Cube.fbx");//立方体Mesh
-        go.AddComponent<MeshRenderer>().material = new Material(Shader.Find("Universal Render Pipeline/Lit"));//设置着色器
+        //GameObject go = new GameObject("Cube");
+        //go.transform.position = Vector3.zero;
+        //go.AddComponent<MeshFilter>().mesh = Resources.GetBuiltinResource<Mesh>("Cube.fbx");//立方体Mesh
+        //go.AddComponent<MeshRenderer>().material = new Material(Shader.Find("Universal Render Pipeline/Lit"));//设置着色器
+
+        //读取游戏对象资源
+        GameObject assetGo = Resources.Load<GameObject>("Chapter03/Script_03_11_Cube");
+        //实例化游戏资源到Hierarchy视图
+        GameObject gameObject = GameObject.Instantiate<GameObject>(assetGo);
+
+        //读取材质资源
+        Material assetMat = Resources.Load<Material>("Chapter03/Script_03_11_Cube_Material");
+        //修改材质
+        gameObject.GetComponent<MeshRenderer>().material = assetMat;
     }
 }
