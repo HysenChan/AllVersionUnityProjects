52bfd6892342e26e3f0453925e3f3665f07fc46d
HysenChan <HysenChan@163.com>
2024-12-23 15:42:24 +0800

    添加字体和刷新。

--- Unity2023.2.20f1c1/Unity3dDesign/Unity3dDesign/Assets/Scripts/Chapter04/Script_04_01.cs
+++ Unity2023.2.20f1c1/Unity3dDesign/Unity3dDesign/Assets/Scripts/Chapter04/Script_04_01.cs
@@ -5,13 +5,46 @@ using UnityEngine.UI;
 
 public class Script_04_01 : MonoBehaviour
 {
+    //private void Start()
+    //{
+    //    Text text = GetComponent<Text>();
+    //    text.text = "Hello World!";
+    //    text.fontSize = 30;
+    //    text.font = Resources.Load<Font>("Chapter04/Font0");
+
+    //    Debug.Log("标签:<size=15><color=green>嵌套标签</color></size>");
+    //}
+
+    //标记某个字体发生了重建
+    private Font m_NeedRebuildFont = null;
+
     private void Start()
     {
-        Text text = GetComponent<Text>();
-        text.text = "Hello World!";
-        text.fontSize = 30;
-        text.font = Resources.Load<Font>("Chapter04/Font0");
+        //监听字体贴图重建事件
+        Font.textureRebuilt += delegate (Font font)
+        {
+            m_NeedRebuildFont = font;
+        };
+    }
 
-        Debug.Log("标签:<size=15><color=green>嵌套标签</color></size>");
+    private void Update()
+    {
+        Debug.Log(m_NeedRebuildFont);
+        if (m_NeedRebuildFont)
+        {
+            //找到当前场景中的所有Text，刷新一下
+            Text[] texts = Object.FindObjectsOfType<Text>();
+            if (texts != null)
+            {
+                foreach (Text text in texts)
+                {
+                    if (text.font == m_NeedRebuildFont)
+                    {
+                        text.FontTextureChanged();
+                    }
+                }
+            }
+            m_NeedRebuildFont = null;
+        }
     }
 }
