1cf1a3fb5c3392f41572cb3b8dfa778d8df3a6e0
HysenChan <HysenChan@163.com>
2024-12-26 18:22:15 +0800

    图集加载

--- Unity2023.2.20f1c1/Unity3dDesign/Unity3dDesign/Assets/Scripts/Chapter04/Script_04_11.cs
+++ Unity2023.2.20f1c1/Unity3dDesign/Unity3dDesign/Assets/Scripts/Chapter04/Script_04_11.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -8,16 +9,40 @@ using UnityEngine.UI;
 
 public class Script_04_11 : MonoBehaviour
 {
-    private void Start()
+    //private void Start()
+    //{
+    //    SpriteAtlas spriteAtlas = Resources.Load<SpriteAtlas>("Chapter04/SpriteAtlas");
+
+    //    //从图集中获取名称为unityicon的Sprite
+    //    Sprite sprite = spriteAtlas.GetSprite("unityicon");
+    //    GetComponent<Image>().sprite = sprite;
+
+    //    //获取图集中的所有Sprite并保存在数组中
+    //    Sprite[] sprites = new Sprite[spriteAtlas.spriteCount];
+    //    spriteAtlas.GetSprites(sprites);
+    //}
+
+    private void OnEnable()
     {
-        SpriteAtlas spriteAtlas = Resources.Load<SpriteAtlas>("Chapter04/SpriteAtlas");
+        SpriteAtlasManager.atlasRequested += AtlasRequested;
+        SpriteAtlasManager.atlasRegistered += AtlasRegistered;
+    }
 
-        //从图集中获取名称为unityicon的Sprite
-        Sprite sprite = spriteAtlas.GetSprite("unityicon");
-        GetComponent<Image>().sprite = sprite;
+    private void OnDisable()
+    {
+        SpriteAtlasManager.atlasRequested -= AtlasRequested;
+        SpriteAtlasManager.atlasRegistered += AtlasRegistered;
+    }
 
-        //获取图集中的所有Sprite并保存在数组中
-        Sprite[] sprites = new Sprite[spriteAtlas.spriteCount];
-        spriteAtlas.GetSprites(sprites);
+    void AtlasRequested(string atlas, Action<SpriteAtlas> action)
+    {
+        Debug.Log($"{atlas}开始加载");
+        //通过Action将加载后的图集对象回调出去
+        action(Resources.Load<SpriteAtlas>(atlas));
+    }
+
+    void AtlasRegistered(SpriteAtlas atlas)
+    {
+        Debug.Log($"{atlas}加载完成");
     }
 }
