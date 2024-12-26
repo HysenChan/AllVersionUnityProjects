27bd90ac2ca403610aa09b1bccc57e81d5cd7a63
HysenChan <HysenChan@163.com>
2024-12-26 18:08:26 +0800

    获取图集资源。

---
+++ Unity2023.2.20f1c1/Unity3dDesign/Unity3dDesign/Assets/Scripts/Chapter04/Script_04_11.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.U2D;
+using UnityEngine.UI;
+
+public class Script_04_11 : MonoBehaviour
+{
+    private void Start()
+    {
+        SpriteAtlas spriteAtlas = Resources.Load<SpriteAtlas>("Chapter04/SpriteAtlas");
+
+        //从图集中获取名称为unityicon的Sprite
+        Sprite sprite = spriteAtlas.GetSprite("unityicon");
+        GetComponent<Image>().sprite = sprite;
+
+        //获取图集中的所有Sprite并保存在数组中
+        Sprite[] sprites = new Sprite[spriteAtlas.spriteCount];
+        spriteAtlas.GetSprites(sprites);
+    }
+}
