60af48afbb7e2c06218921b1a5901e8c215b1fe5
HysenChan <HysenChan@163.com>
2024-12-30 15:09:33 +0800

    3D事件

--- Unity2023.2.20f1c1/Unity3dDesign/Unity3dDesign/Assets/Scripts/Chapter04/Script_04_15.cs
+++ Unity2023.2.20f1c1/Unity3dDesign/Unity3dDesign/Assets/Scripts/Chapter04/Script_04_15.cs
@@ -10,36 +10,45 @@ using UnityEngine.UI;
 
 public class Script_04_15 : MonoBehaviour
 {
-    //无返回值
-    public delegate void MyDelegate(int x, int y);
-    public MyDelegate Delegate;
-    public Action<int, int> Action;
+    ////无返回值
+    //public delegate void MyDelegate(int x, int y);
+    //public MyDelegate Delegate;
+    //public Action<int, int> Action;
 
-    //有返回值
-    public delegate int MyDelegate2(int x, int y);
-    public MyDelegate2 Delegate2;
-    public Func<int, int, int> Func;
+    ////有返回值
+    //public delegate int MyDelegate2(int x, int y);
+    //public MyDelegate2 Delegate2;
+    //public Func<int, int, int> Func;
 
+    //private void Awake()
+    //{
+    //    Delegate += (x, y) => { };
+    //    Action += (x, y) => { };
+
+    //    Delegate2 += (x, y) =>
+    //    {
+    //        return x + y;
+    //    };
+    //    Func += (x, y) =>
+    //    {
+    //        return x + y;
+    //    };
+
+    //    //执行
+    //    Delegate?.Invoke(1, 1);
+    //    Action?.Invoke(1, 1);
+
+    //    //返回值
+    //    int a = Delegate2.Invoke(1, 1);
+    //    int b = Func.Invoke(1, 1);
+    //}
+
+    public GameObject Cube;
     private void Awake()
     {
-        Delegate += (x, y) => { };
-        Action += (x, y) => { };
-
-        Delegate2 += (x, y) =>
+        UGUIEventListener.Get(Cube).onClick += (go) =>
         {
-            return x + y;
+            Debug.Log("模型点击事件");
         };
-        Func += (x, y) =>
-        {
-            return x + y;
-        };
-
-        //执行
-        Delegate?.Invoke(1, 1);
-        Action?.Invoke(1, 1);
-
-        //返回值
-        int a = Delegate2.Invoke(1, 1);
-        int b = Func.Invoke(1, 1);
     }
 }
