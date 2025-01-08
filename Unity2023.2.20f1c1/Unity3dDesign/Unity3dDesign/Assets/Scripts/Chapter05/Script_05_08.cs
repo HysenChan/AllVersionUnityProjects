bd988588a66623485fef9b4683bf23784d1a6eee
HysenChan <HysenChan@163.com>
2025-01-08 18:01:15 +0800

    ListView组件。
    只会给当前需要显示的数的子对象进行赋值。

---
+++ Unity2023.2.20f1c1/Unity3dDesign/Unity3dDesign/Assets/Scripts/Chapter05/Script_05_08.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.UIElements.Experimental;
+
+public class Script_05_08 : MonoBehaviour
+{
+    public VisualTreeAsset ItemAsset;
+    private void Start()
+    {
+        UIDocument document = GetComponent<UIDocument>();
+        var root = document.rootVisualElement;
+
+        //准备数据阶段
+        const int itemCount = 10000;
+        List<string> items = new List<string>(itemCount);
+        for (int i = 0; i <= itemCount; i++)
+        {
+            items.Add($"我是第{i}个<sprite=3>");
+        }
+
+        ListView listView = root.Q<ListView>();
+
+        Func<VisualElement> makeItem = () =>
+        {
+            //当出现在屏幕上时开始克隆对象
+            return ItemAsset.CloneTree();
+        };
+
+        Action<VisualElement, int> bindItem = (e, i) =>
+        {
+            //屏幕上的对象已经准备好开始刷新数据
+            e.Q<Label>().text = listView.itemsSource[i].ToString();
+        };
+
+        listView.selectedIndicesChanged += (indexes) =>
+        {
+            //选择某个元素调用
+            foreach (var index in indexes)
+            {
+                Debug.Log($"第{index}个被选择了！");
+            }
+        };
+
+        //设置每个Item的固定高度
+        listView.fixedItemHeight = 100;
+        //设置原始数据
+        listView.itemsSource = items;
+        listView.makeItem = makeItem;
+        listView.bindItem = bindItem;
+
+        var button = root.Q<Button>();
+        button.clicked += () =>
+        {
+            Debug.Log("Button clicked");
+            listView.ScrollToItem(500);//跳转到第500个元素
+        };
+    }
+}
