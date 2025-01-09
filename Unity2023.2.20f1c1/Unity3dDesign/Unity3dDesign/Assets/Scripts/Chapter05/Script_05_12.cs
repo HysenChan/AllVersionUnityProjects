9bcee5812fab737d62d8315e292a54630fb8f548
HysenChan <HysenChan@163.com>
2025-01-09 15:19:16 +0800

    焦点事件

--- Unity2023.2.20f1c1/Unity3dDesign/Unity3dDesign/Assets/Scripts/Chapter05/Script_05_12.cs
+++ Unity2023.2.20f1c1/Unity3dDesign/Unity3dDesign/Assets/Scripts/Chapter05/Script_05_12.cs
@@ -7,5 +7,27 @@ using UnityEngine.UIElements.Experimental;
 
 public class Script_05_12 : MonoBehaviour
 {
-    
+    private void Start()
+    {
+        UIDocument document = GetComponent<UIDocument>();
+        var root = document.rootVisualElement;
+
+        var textFiled = root.Q<TextField>();
+        textFiled.RegisterCallback<FocusOutEvent>((evt) =>
+        {
+            Debug.Log("失去焦点之前发送");
+        });
+        textFiled.RegisterCallback<FocusInEvent>((evt) =>
+        {
+            Debug.Log("获取焦点之前发送");
+        });
+        textFiled.RegisterCallback<BlurEvent>((evt) =>
+        {
+            Debug.Log("失去焦点之后发送");
+        });
+        textFiled.RegisterCallback<FocusEvent>((evt) =>
+        {
+            Debug.Log("获取焦点之后发送");
+        });
+    }
 }
