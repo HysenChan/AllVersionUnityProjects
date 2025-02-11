50b5f84353f8c9cf97d176c2f501c0d621617be2
HysenChan <HysenChan@163.com>
2025-02-11 18:05:24 +0800

    异步加载

--- Unity2023.2.20f1c1/Unity3dDesign/Unity3dDesign/Assets/Scripts/Chapter07/Script_07_02.cs
+++ Unity2023.2.20f1c1/Unity3dDesign/Unity3dDesign/Assets/Scripts/Chapter07/Script_07_02.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,19 +9,52 @@ public class Script_07_02 : MonoBehaviour
     private AsyncOperation m_Async;
     private void Start()
     {
-        //异步加载场景
-        m_Async = SceneManager.LoadSceneAsync("Chapter07_01", LoadSceneMode.Single);
-        m_Async.completed += (async) =>
+        ////异步加载场景
+        //m_Async = SceneManager.LoadSceneAsync("Chapter07_01", LoadSceneMode.Single);
+        //m_Async.completed += (async) =>
+        //{
+        //    Debug.Log($"加载完成:{m_Async.progress}");
+        //};
+        //避免当前脚本在切换场景时被剔除，通过DontDestroyOnLoad设置
+        DontDestroyOnLoad(this);
+        SceneMod.LoadScene("Chapter07_01", () =>
         {
-            Debug.Log($"加载完成:{m_Async.progress}");
-        };
+            Debug.Log($"场景加载完成");
+        });
     }
 
-    private void Update()
+    //IEnumerator LoadScene(string sceneName, System.Action finish)
+    //{
+    //    AsyncOperation async = SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Single);
+    //    yield return async;
+    //    finish?.Invoke();
+    //}
+
+    //private void Update()
+    //{
+    //    if (!m_Async.isDone)
+    //    {
+    //        Debug.Log($"加载进度：{m_Async.progress}");
+    //    }
+    //}
+    public class SceneMod
     {
-        if (!m_Async.isDone)
+        public static void LoadScene(string name, Action finish)
         {
-            Debug.Log($"加载进度：{m_Async.progress}");
+            //新场景和当前场景一致时，直接返回加载成功事件
+            if (SceneManager.GetActiveScene().name == name)
+            {
+                finish?.Invoke();
+                return;
+            }
+            //加载一个空场景
+            SceneManager.LoadScene("Empty");
+            //异步加载场景
+            var async = SceneManager.LoadSceneAsync(name, LoadSceneMode.Single);
+            async.completed += (async) =>
+            {
+                finish?.Invoke();
+            };
         }
     }
 }
